e09375f3d9c3c678b3c12369558abc23553d5d62
mn-6 <michal.nocun@studenci.collegiumwitelona.pl>
2026-03-17 19:20:19 +0100

    clean setup
    axios+vite+tw+rr

---
+++ src/router/router.jsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+export const router = createBrowserRouter([
+    { path: "/", element:<Dashboard/>},
+]);
