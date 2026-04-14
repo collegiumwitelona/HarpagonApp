62980c717d98988fd035498ee1f9dd441a8a7f95
Stanislav Zhuk <stanislav.zhuk@studenci.collegiumwitelona.pl>
2026-04-15 00:06:50 +0200

    0 to null change

--- Application/DTO/Requests/Filtering/FilteringRequest.cs
+++ Application/DTO/Requests/Filtering/FilteringRequest.cs
@@ -12,7 +12,7 @@ namespace Application.DTO.Requests.Filtering
         public string? CategoryName { get; set; } = null;
         public DateOnly? FromDate { get; set; } = null;
         public DateOnly? ToDate { get; set; } = null;
-        public decimal? FromAmount { get; set; } = 0;
+        public decimal? FromAmount { get; set; } = null;
         public decimal? ToAmount { get; set; } = null;
     }
 }
