1d54087ce9df12fac701c773258bbf2a34d693e1
Stanislav Zhuk <stanislav.zhuk@studenci.collegiumwitelona.pl>
2026-04-18 02:06:56 +0200

    Deploy fix (#21)
    * vite api url
    
    * renderyaml

--- src/services/api.js
+++ src/services/api.js
@@ -6,6 +6,7 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+console.log(import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use((config) => {
   const rawToken = localStorage.getItem("token") || "";
