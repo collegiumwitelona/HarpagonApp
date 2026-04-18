d42e6e3a10ae0377a3565a81648bf92d4b7148e1
Julia Kozłowska <1juliakozlowska@gmail.com>
2026-04-18 23:40:44 +0200

    about page. renamed harpagon-front to frontend in compose.yaml

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,30 @@
+import React from "react";
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { useLanguage } from '../context/LanguageContext';
+
+
 export default function About() {
+    const { t } = useLanguage();
+    
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold">About</h1>
-    </div>
+      <div className="h-screen flex flex-col bg-slate-50 font-sans text-slate-900 overflow-hidden">
+          <Navbar />
+          <main className="grow flex flex-col justify-center items-center px-6 bg-hero-blur w-full text-center space-y-12">
+              <h1 className="text-4xl md:text-6xl font-bold tracking-tight text-white!">
+                  About <br />
+              </h1>
+              <p className="text-lg md:text-xl text-white! leading-relaxed max-w-2xl pt-10">
+                  Harpagon to wygodna aplikacja stworzona z myslą o zarządzaniu budżetem domowym. <br /> <br />
+                  Projekt studencji wykonany przez:
+                  <ul>
+                      <li>Julia Kozłowska 44922</li>
+                      <li>Stanislav Zhuk 44963</li>
+                      <li>Michał Nocuń 40669</li>
+                  </ul>
+              </p>
+          </main>
+          <Footer />
+      </div>
   );
 }
